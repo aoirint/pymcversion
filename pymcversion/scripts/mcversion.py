b1d8f881f13f35b7a9e8607b053bfa86bfa0e4c0
aoirint <aoirint@gmail.com>
2021-10-23 00:40:40 +0900

    bedrock-server: linux-version, linux-url

--- pymcversion/scripts/mcversion.py
+++ pymcversion/scripts/mcversion.py
@@ -4,8 +4,11 @@ from pymcversion import (
     get_ios_store_lookup,
 )
 
-def command_bedrock_server_version(args):
+def command_bedrock_server_linux_version(args):
     print(get_bedrock_server_latest_version().linux.version)
+
+def command_bedrock_server_linux_url(args):
+    print(get_bedrock_server_latest_version().linux.url)
 
 def command_java_version(args):
     print(get_java_version_manifest().latest.release)
@@ -19,10 +22,12 @@ def main():
     subparsers = parser.add_subparsers()
 
     parser_bedrock = subparsers.add_parser('bedrock-server')
-    parser_bedrock.set_defaults(handler=command_bedrock_server_version)
+    parser_bedrock.set_defaults(handler=command_bedrock_server_linux_version)
     subparsers_bedrock = parser_bedrock.add_subparsers()
-    parser_bedrock_version = subparsers_bedrock.add_parser('version')
-    parser_bedrock_version.set_defaults(handler=command_bedrock_server_version)
+    parser_bedrock_linux_version = subparsers_bedrock.add_parser('linux-version')
+    parser_bedrock_linux_version.set_defaults(handler=command_bedrock_server_linux_version)
+    parser_bedrock_linux_url = subparsers_bedrock.add_parser('linux-url')
+    parser_bedrock_linux_url.set_defaults(handler=command_bedrock_server_linux_url)
 
     parser_java = subparsers.add_parser('java')
     parser_java.set_defaults(handler=command_java_version)
