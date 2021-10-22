f40acddffc630e7c60541b0b50d6a50af385c7b8
aoirint <aoirint@gmail.com>
2021-10-23 00:06:37 +0900

    rm type import

--- pymcversion/__init__.py
+++ pymcversion/__init__.py
@@ -1,9 +1,8 @@
 __VERSION__ = '20211022.1'
 
-from .bedrock_server import get_bedrock_server_latest_version
+from .bedrock_server import (
+    get_bedrock_server_latest_version,
+)
 from .java_version_manifest import (
     get_java_version_manifest,
-    VersionManifest,
-    VersionManifestLatest,
-    VersionManifestVersion,
 )
