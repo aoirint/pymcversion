fec9cc9ed8f300f75b25c0fb2dc6767f654d48e5
aoirint <aoirint@gmail.com>
2021-10-22 23:16:18 +0900

    package

---
+++ pymcversion/__init__.py
@@ -0,0 +1,3 @@
+__VERSION__ = '20211022.1'
+
+from .bedrock_server import get_bedrock_server_latest_version
---
+++ pymcversion/bedrock_server.py
@@ -0,0 +1,18 @@
+import re
+import requests
+
+def get_bedrock_server_latest_version() -> str:
+    timeout = 3
+    useragent = 'aoirint/pymcversion'
+    headers = {
+        'User-Agent': useragent,
+    }
+
+    res = requests.get('https://www.minecraft.net/en-us/download/server/bedrock', headers=headers, timeout=timeout)
+    html = res.text
+
+    # https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40.06.zip
+    m = re.search(r'"https://minecraft\.azureedge\.net/bin-linux/bedrock-server-(.+?)\.zip"', html)
+
+    version = m.group(1)
+    return version
